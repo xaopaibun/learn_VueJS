33db3e5bbdc03a17db2d16b4c41c5b5c45adc602
xaopaibun <vanquy33338888@gmail.com>
2022-09-20 17:09:03 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <Todo :number=number @clicked-submit="handleSubmitParent"></Todo>
+  <div>name: {{name}}</div>
+  <div v-for="item in datas" v-bind:key=item.id>
+    {{item.class}}
+  </div>
+</template>
+
+<script>
+import Todo from './components/Todo.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Todo
+  },
+  data: () => {
+    return {
+      name: '',
+      number: 200,
+      datas: [
+        {
+          id: '1',
+          class: 'D13CNPM6'
+        },
+        {
+          id: '2',
+          class: 'D13CNPM7'
+        }
+      ]
+    }
+  },
+  methods: {
+    handleSubmitParent: function (name) {
+      this.name = name;
+    }
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Todo.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="hello">
+        <h1>number: {{ number }}</h1>
+        <button @click="handleSubmit">Submit</button>
+    </div>
+</template>
+  
+<script>
+export default {
+    // eslint-disable-next-line vue/multi-word-component-names
+    name: 'Todo',
+    props: {
+        number: Number
+    },
+    methods: {
+        handleSubmit: function () {
+            this.$emit('clicked-submit', 'Pham Jin')
+        }
+    }
+}
+</script>
+  
+  <!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+    margin: 40px 0 0;
+}
+
+ul {
+    list-style-type: none;
+    padding: 0;
+}
+
+li {
+    display: inline-block;
+    margin: 0 10px;
+}
+
+a {
+    color: #42b983;
+}
+</style>
+  
