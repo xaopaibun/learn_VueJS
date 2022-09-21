51f32fef54c9094587f93db0c4ce40f9f8e84dae
xaopaibun <vanquy33338888@gmail.com>
2022-09-21 17:23:00 +0700

    feat: call api

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,20 @@
   <img alt="Vue logo" src="./assets/logo.png">
   <Todo :number=number @clicked-submit="handleSubmitParent"></Todo>
   <div>name: {{name}}</div>
-  <div v-for="item in datas" v-bind:key=item.id>
-    {{item.class}}
-  </div>
+  <h1>Data length: {{listProduct.length}}</h1>
+  <a-table :dataSource="listProduct" :columns="columns">
+    <template v-slot:image-column="image" >
+      <td>
+        <img :src="image" style="width: 50px; height: 50px" />
+      </td>
+    </template>
+  </a-table>
 </template>
 
 <script>
-import Todo from './components/Todo.vue'
-
+import Todo from './components/Todo.vue';
+import axios from 'axios';
+import 'ant-design-vue/dist/antd.css';
 export default {
   name: 'App',
   components: {
@@ -19,22 +25,51 @@ export default {
     return {
       name: '',
       number: 200,
-      datas: [
-        {
-          id: '1',
-          class: 'D13CNPM6'
-        },
-        {
-          id: '2',
-          class: 'D13CNPM7'
-        }
-      ]
+      listProduct: [],
+      columns: [
+          {
+            title: 'title',
+            dataIndex: 'title',
+            key: 'title',
+          },
+          {
+            title: 'price',
+            dataIndex: 'price',
+            key: 'price',
+          },
+          {
+            title: 'description',
+            dataIndex: 'description',
+            key: 'description',
+          },
+          {
+            title: 'category',
+            dataIndex: 'category',
+            key: 'category',
+          },
+          {
+            title: 'image',
+            dataIndex: 'image',
+            key: 'image',
+            scopedSlots: { customRender: "image-column" },
+          },
+        ],
     }
   },
   methods: {
     handleSubmitParent: function (name) {
       this.name = name;
     }
+  },
+  mounted() {
+    console.log(`the component is now mounted.`);
+    (async () => {
+      const { data } = await axios.get("https://fakestoreapi.com/products");
+      this.listProduct = data;
+    })()
+  },
+  created() {
+    console.log(`the component is now created.`)
   },
 }
 </script>
